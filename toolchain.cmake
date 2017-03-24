017ed5955c801c350f1b8869c01db2735a643b6c
Ezio <ezio_zhang@outlook.com>
2017-03-24 15:36:13 +0800

    Create toolchain.cmake

---
+++ toolchain.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_C_COMPILER "aarch64-elf-gcc")
+SET(CMAKE_C_FLAGS_DEBUG " -fno-common -fno-builtin -ffreestanding  -nostdinc ")
+set(CMAKE_EXE_LINKER_FLAGS_DEBUG " -T ${CMAKE_SOURCE_DIR}/my.lds -static ")
+SET(CMAKE_C_LINK_EXECUTABLE "aarch64-elf-ld  <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS>  -o <TARGET>")
+set(CMAKE_BUILD_TYPE Debug CACHE STRING "Choose the type of build." FORCE)
