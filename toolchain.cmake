7d0557823e059b02c06fe85d47aa3e710166458f
ezio <ezio_zhang@outlook.com>
2017-03-27 00:29:03 +0800

    compile ok

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,4 +1,1 @@
-SET(CMAKE_C_FLAGS_DEBUG " -fno-common -fno-builtin -ffreestanding  -nostdinc ")
-set(CMAKE_EXE_LINKER_FLAGS_DEBUG " -T ${CMAKE_SOURCE_DIR}/my.lds -static ")
-SET(CMAKE_C_LINK_EXECUTABLE "/usr/bin/ld  <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS>  -o <TARGET>")
-set(CMAKE_BUILD_TYPE Debug CACHE STRING "Choose the type of build." FORCE)
+SET(CMAKE_C_LINK_EXECUTABLE "/usr/bin/ld <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS>  -o <TARGET>")
